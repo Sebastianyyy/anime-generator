c96dc1ecaeae9914501318f0898b25f2bbf69261
Sebastian Musial (Sebastianyyy) <64799476+Sebastianyyy@users.noreply.github.com>
2023-09-20 18:57:03 +0200

    added training loop and loss function

--- main.py
+++ main.py
@@ -41,50 +41,50 @@ if __name__=='main':
         netG.parameters(), lr=config.lr, betas=(config.beta1, 0.999))
 
     G_losses = []
-D_losses = []
-num_epochs = 1
-netD.train()
-netG.train()
-for epoch in range(num_epochs):
-    for i, data in enumerate(data_loader, 0):
-        # TRAINING DISCRIMINATOR REAL
-        netD.zero_grad()
-        label = torch.full((config.batch_size,), real_label,
-                           dtype=torch.float).to(config.device)
-        logits = netD(data.to(config.device)).view(-1)
-        error_D_real = loss(logits, label)
-        error_D_real.backward()
-        E_Dx = logits.mean().item()
+    D_losses = []
+    num_epochs = 1
+    netD.train()
+    netG.train()
+    for epoch in range(num_epochs):
+        for i, data in enumerate(data_loader, 0):
+            # TRAINING DISCRIMINATOR REAL
+            netD.zero_grad()
+            label = torch.full((config.batch_size,), real_label,
+                            dtype=torch.float).to(config.device)
+            logits = netD(data.to(config.device)).view(-1)
+            error_D_real = loss(logits, label)
+            error_D_real.backward()
+            E_Dx = logits.mean().item()
 
-        # TRAINING DISCRIMINATOR FAKE
-        noise = torch.randn(config.batch_size, config.nz,
-                            1, 1).to(config.device)
-        fake = netG(noise)
-        label.fill_(fake_label)
-        logits = netD(fake.detach()).view(-1)
-        error_D_fake = loss(logits, label)
-        error_D_fake.backward()
-        E_DGz_1 = logits.mean().item()
-        errorD = error_D_real + error_D_fake
-        optimizerD.step()
+            # TRAINING DISCRIMINATOR FAKE
+            noise = torch.randn(config.batch_size, config.nz,
+                                1, 1).to(config.device)
+            fake = netG(noise)
+            label.fill_(fake_label)
+            logits = netD(fake.detach()).view(-1)
+            error_D_fake = loss(logits, label)
+            error_D_fake.backward()
+            E_DGz_1 = logits.mean().item()
+            errorD = error_D_real + error_D_fake
+            optimizerD.step()
 
-        # TRAINING GENERATOR
-        netG.zero_grad()
-        label.fill_(real_label)
-        logits = netD(fake).view(-1)
-        errorG = loss(logits, label)
-        errorG.backward()
-        E_DGz_2 = logits.mean().item()
-        optimizerG.step()
+            # TRAINING GENERATOR
+            netG.zero_grad()
+            label.fill_(real_label)
+            logits = netD(fake).view(-1)
+            errorG = loss(logits, label)
+            errorG.backward()
+            E_DGz_2 = logits.mean().item()
+            optimizerG.step()
 
-        if i % 50 == 0:
-            print(f"""[{epoch}/{num_epochs}] [{i}/{len(data_loader)}] Loss Discriminator Real: {error_D_real:.2f}
-            Loss Discriminator Fake: {error_D_fake:.2f} 
-            Loss Discriminator: {errorD:.2f} 
-            Loss Generator: {errorG:.2f} 
-            Mean Dx: {E_Dx:.2f}
-            Mean Dgz_1: {E_DGz_1:.2f} 
-            Mean Dgz_2: {E_DGz_2:.2f}""")
+            if i % 50 == 0:
+                print(f"""[{epoch}/{num_epochs}] [{i}/{len(data_loader)}] Loss Discriminator Real: {error_D_real:.2f}
+                Loss Discriminator Fake: {error_D_fake:.2f} 
+                Loss Discriminator: {errorD:.2f} 
+                Loss Generator: {errorG:.2f} 
+                Mean Dx: {E_Dx:.2f}
+                Mean Dgz_1: {E_DGz_1:.2f} 
+                Mean Dgz_2: {E_DGz_2:.2f}""")
 
-        G_losses.append(errorG.item())
-        D_losses.append(errorD.item())
+            G_losses.append(errorG.item())
+            D_losses.append(errorD.item())
